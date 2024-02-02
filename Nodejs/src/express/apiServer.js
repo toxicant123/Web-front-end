89e711d78a4d501ebd8fc05c828d2a907e32b5cd
toxicant123 <13655847550@163.com>
2024-02-03 00:16:44 +0800

    0.2.19 Nodejs

--- Nodejs/src/express/apiServer.js
+++ Nodejs/src/express/apiServer.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 const router = require('./router/apiRouter')
 
-app.use(router)
+app.use(cors())
+
+app.use(express.json())
+
+app.use('/api', router)
 
 app.listen(80, () => {
     console.log('express server running at http://127.0.0.1')
--- Nodejs/src/express/router/apiRouter.js
+++ Nodejs/src/express/router/apiRouter.js
@@ -2,4 +2,22 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/get', (req, res) => {
+    const query = req.query
+    res.send({
+        status: 0,
+        msg: 'GET请求成功',
+        data: query
+    })
+})
+
+router.post('/post', (req, res) => {
+    const body = req.body
+    res.send({
+        status: 0,
+        msg: 'POST请求成功',
+        data: body
+    })
+})
+
 module.exports = router
