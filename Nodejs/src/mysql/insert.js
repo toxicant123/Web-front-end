9c6f60b9996a3f8347ddd09791d63da6d6f238d5
toxicant123 <13655847550@163.com>
2024-02-03 17:30:41 +0800

    0.2.24 Nodejs

--- Nodejs/src/mysql/insert.js
+++ Nodejs/src/mysql/insert.js
@@ -1,13 +1,31 @@
 const db = require('./db/db')
 
+// const user = {
+//     username: 'Spider-Man',
+//     password: 'pcc123'
+// }
+//
+// const sqlStr = 'insert into users(username, password) value (?, ?)'
+//
+// db.query(sqlStr, [user.username, user.password], (err, results) => {
+//     if (err) {
+//         console.log(err.message)
+//         return
+//     }
+//
+//     if (results.affectedRows === 1) {
+//         console.log('插入数据成功')
+//     }
+// })
+
 const user = {
-    username: 'Spider-Man',
-    password: 'pcc123'
+    username: 'Spider-Man2',
+    password: 'pcc4312'
 }
 
-const sqlStr = 'insert into users(username, password) value (?, ?)'
+const sqlStr = 'insert into users set ?';
 
-db.query(sqlStr, [user.username, user.password], (err, results) => {
+db.query(sqlStr, user, (err, results) => {
     if (err) {
         console.log(err.message)
         return
