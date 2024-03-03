41281860558e0bfc832f150ebb2dcff265e0e5d7
toxicant123 <13655847550@163.com>
2024-03-03 18:47:53 +0800

    0.4.125 Vue

---
+++ Vue/Vue2/src/project/vuex_demo/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="app">
+        <h1>根组件
+            - {{ title }}
+            - {{ count }}
+        </h1>
+        <input type="text" :value="count" @input="changeCount">
+        <Son1></Son1>
+        <hr>
+        <Son2></Son2>
+    </div>
+</template>
+
+<script>
+import Son1 from './components/Son1.vue'
+import Son2 from './components/Son2.vue'
+import {mapState} from "vuex"
+
+
+export default {
+    name: 'app',
+    methods: {
+        changeCount(event) {
+            this.$store.commit('changeCount', event.target.value)
+        }
+    },
+    data: function () {
+        return {}
+    },
+    components: {
+        Son1,
+        Son2
+    },
+    created() {
+        console.log(this.$store)
+    },
+    computed: {
+        ...mapState(['count', 'title'])
+    }
+}
+</script>
+
+<style>
+#app {
+    width: 600px;
+    margin: 20px auto;
+    border: 3px solid #ccc;
+    border-radius: 3px;
+    padding: 10px;
+}
+</style>
---
+++ Vue/Vue2/src/project/vuex_demo/src/components/Son2.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="box">
+        <h2>Son2 子组件</h2>
+        从vuex中获取的值：title -> <label>{{ title }}</label>
+        <br/>
+        从vuex中获取的值：count -> <label>{{ count }}</label>
+        <br/>
+        <button @click="handleDelCount(1)">值 - 1</button>
+        <button @click="handleDelCount(5)">值 - 5</button>
+        <button @click="handleDelCount(10)">值 - 10</button>
+        <button @click="changeSon2Title">改标题</button>
+    </div>
+</template>
+
+<script>
+import {mapState, mapMutations} from "vuex";
+
+export default {
+    name: 'Son2Com',
+    methods: {
+        ...mapMutations(['delCount', 'changeTitle']),
+        handleDelCount(num) {
+            this.delCount(num)
+        },
+        changeSon2Title() {
+            this.changeTitle('Son2 changed the title')
+        }
+    },
+    computed: {
+        ...mapState(['count', 'title'])
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.box {
+    border: 3px solid #ccc;
+    width: 400px;
+    padding: 10px;
+    margin: 20px;
+}
+
+h2 {
+    margin-top: 10px;
+}
+</style>
