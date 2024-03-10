c9e096ac11052d34b0669981df6f1c00d5aa8efe
toxicant123 <13655847550@163.com>
2024-03-10 10:10:39 +0800

    0.4.151 Vue

--- Vue/Vue2/src/project/hm-shopping/src/utils/request.js
+++ Vue/Vue2/src/project/hm-shopping/src/utils/request.js
@@ -6,11 +6,25 @@ const instance = axios.create({
   timeout: 5000
 })
 
+instance.interceptors.request.use(config => {
+  Toast.loading({
+    message: '加载中',
+    forbidClick: true,
+    duration: 0
+  })
+
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
 instance.interceptors.response.use(response => {
   const res = response.data
   if (res.status !== 200) {
     Toast(res.message)
     return Promise.reject(res.message)
+  } else {
+    Toast.clear()
   }
   return res
 }, error => {
