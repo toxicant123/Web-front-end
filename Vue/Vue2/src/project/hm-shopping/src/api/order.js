07ddbffa6ffa3cbee25dad0a0ce7faa4830058e3
toxicant123 <13655847550@163.com>
2024-03-15 23:31:23 +0800

    0.4.172 Vue

--- Vue/Vue2/src/project/hm-shopping/src/api/order.js
+++ Vue/Vue2/src/project/hm-shopping/src/api/order.js
@@ -1,5 +1,13 @@
 import request from '@/utils/request'
 
-export const checkOrder = () => {
-  return request.get('/checkout/order')
+export const checkOrder = (mode, obj) => {
+  return request.get('/checkout/order', {
+    params: {
+      mode,
+      delivery: 10,
+      couponId: 0,
+      isUsePoints: 0,
+      ...obj
+    }
+  })
 }
