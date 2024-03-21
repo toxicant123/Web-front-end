cb96c751aadeb475d494ee1684cb0576b01a2c02
toxicant123 <13655847550@163.com>
2024-03-21 20:45:13 +0800

    0.4.192 Vue

--- Vue/Vue3/src/vue3-pinia-demo/src/App.vue
+++ Vue/Vue3/src/vue3-pinia-demo/src/App.vue
@@ -2,17 +2,37 @@
 import Son1Com from "@/components/Son1Com.vue";
 import Son2Com from "@/components/Son2Com.vue";
 
-import { useCounterStore } from '@/store/counter'
+import {useCounterStore} from '@/store/counter'
+import {useChannelStore} from "@/store/channel.js";
+import {ref} from "vue";
+import {storeToRefs} from "pinia";
 
-const counterStore = useCounterStore();
+const counterStore = useCounterStore()
+const channelStore = useChannelStore();
+
+// 使用storeToRefs后解构可以保留响应式
+const { count, msg } = storeToRefs(counterStore)
+
+const list = ref([]);
+
+const pushButton = async () => {
+    await channelStore.getList()
+    list.value = channelStore.channelList
+}
 
 </script>
 
 <template>
     <div>
-        <h3>App.vue根组件 - {{ counterStore.count }} - {{ counterStore.msg }} - double: {{ counterStore.double }}</h3>
-        <Son1Com></Son1Com>
-        <Son2Com></Son2Com>
+        <h3>App.vue根组件 - {{ count }} - {{ msg }} - double:
+            {{ counterStore.double }}</h3>
+        <Son1Com/>
+        <Son2Com/>
+        <hr>
+        <button @click="pushButton">获取频道数据</button>
+        <ul>
+            <li v-for="item in list" :key="item.id">{{ item.name }}</li>
+        </ul>
     </div>
 </template>
 
