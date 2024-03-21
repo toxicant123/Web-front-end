e9c4b65ed051d6e45f6e96266367db98e8921475
toxicant123 <13655847550@163.com>
2024-03-21 23:52:11 +0800

    0.4.195 Vue

--- Vue/Vue3/src/Vue3-big-event-admin/src/App.vue
+++ Vue/Vue3/src/Vue3-big-event-admin/src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
 
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+const goList = () => {
+  router.push({
+    path: '/list'
+  })
+}
+
 </script>
 
 <template>
-  <div>this is App</div>
+  <div>
+    this is App
+    <button @click="$router.push('/home')">跳首页</button>
+    <button @click="goList">跳列表页</button>
+  </div>
 </template>
 
 <style scoped>
