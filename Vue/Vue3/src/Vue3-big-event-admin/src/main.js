1975836c154d70a33eb1828f19b8311ce406bdfa
toxicant123 <13655847550@163.com>
2024-03-22 23:53:30 +0800

    0.4.198 Vue

--- Vue/Vue3/src/Vue3-big-event-admin/src/main.js
+++ Vue/Vue3/src/Vue3-big-event-admin/src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import persist from 'pinia-plugin-persistedstate'
+
 import App from './App.vue'
 import router from './router'
 
 import '@/assets/main.scss'
+import pinia from '@/stores/index.js'
 
 const app = createApp(App)
 
-app.use(createPinia().use(persist))
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- Vue/Vue3/src/Vue3-big-event-admin/src/stores/user.js
+++ Vue/Vue3/src/Vue3-big-event-admin/src/stores/user.js
@@ -1,22 +1,26 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useUserStore = defineStore('user', () => {
-  const token = ref('')
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const token = ref('')
 
-  const setToken = newToken => {
-    token.value = newToken
-  }
+    const setToken = (newToken) => {
+      token.value = newToken
+    }
 
-  const removeToken = () => {
-    token.value = ''
-  }
+    const removeToken = () => {
+      token.value = ''
+    }
 
-  return {
-    token,
-    setToken,
-    removeToken
+    return {
+      token,
+      setToken,
+      removeToken
+    }
+  },
+  {
+    persist: true
   }
-}, {
-  persist: true
-})
+)
