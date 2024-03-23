52c73f531dadd947973f205f397cbf75e1817010
toxicant123 <13655847550@163.com>
2024-03-23 23:29:03 +0800

    0.4.206 Vue

--- Vue/Vue3/src/Vue3-big-event-admin/src/views/article/ArticleChannel.vue
+++ Vue/Vue3/src/Vue3-big-event-admin/src/views/article/ArticleChannel.vue
@@ -1,11 +1,25 @@
 <script setup>
-
+import PageContainer from '@/components/PageContainer.vue'
 </script>
 
 <template>
-  频道管理
+  <PageContainer title="文章分类">
+    <template #extra>
+      <el-button>按钮</el-button>
+    </template>
+    <div>文章分类主体部分</div>
+  </PageContainer>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.page-container {
+  min-height: 100%;
+  box-sizing: border-box;
 
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
 </style>
--- Vue/Vue3/src/Vue3-big-event-admin/src/views/article/ArticleManage.vue
+++ Vue/Vue3/src/Vue3-big-event-admin/src/views/article/ArticleManage.vue
@@ -1,11 +1,25 @@
 <script setup>
-
+import PageContainer from '@/components/PageContainer.vue'
 </script>
 
 <template>
-  文章管理
+  <PageContainer title="文章管理">
+    <template #extra>
+      <el-button>添加文章</el-button>
+    </template>
+    <div>添加文章主体部分</div>
+  </PageContainer>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.page-container {
+  min-height: 100%;
+  box-sizing: border-box;
 
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
 </style>
